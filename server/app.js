a3cd3229390acf22a6535f352bcbc40ee9ee9ccd
Pravalika-26 <pravalikavollala08@gmail.com>
2024-08-29 12:03:54 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const studentRouter = require('./routes/students')
 
 
 //const url = 'mongodb://127.0.0.1:27017/CBIT'
-const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+const url="mongodb+srv://pravalikavollala08:4508@ead.rnyhl.mongodb.net/?retryWrites=true&w=majority&appName=ead"
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
@@ -19,7 +20,7 @@ console.log('connected...')
 app.use(express.json())
 
 app.use('/students',studentRouter)
-app.listen(9000, () =>
+app.listen(9008, () =>
 {
 console.log('Server started')
 })
